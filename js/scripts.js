14e1c4b98072cdb1cdb00b724bbc08121b2fbe67
William Kulha <kulha.william@gmail.com>
2018-09-13 13:27:44 -0400

    Add javascript to show a celebrity matching user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+var addError = function(){
+  $('div#genderForm').addClass("has-error")
+  $('div#genderForm').append('<span id="helpBlock2" class="help-block">Please enter either men or women.</span>')
+}
+
+
 $(document).ready(function(){
   $("form#quizInput").submit(function(event){
     event.preventDefault();
@@ -6,11 +12,27 @@ $(document).ready(function(){
     var wildness = $('input:radio[name=wildRadios]:checked').val();
 
     if (gender) {
-      if (gender === male &&& age == 1) {
-        $('#matt').show();
-        $('#danniel').slideup();
-
+      if (gender === 'men' && age === '1' && wildness <= '2') {
+        $('#shemar').slideDown();
+      } else if (gender === 'men' && age === '2'&& wildness === '1') {
+        $('#daniel').slideDown();
+      } else if (gender === 'men' && age === '2' && wildness <= '2') {
+        $('#idris').slideDown();
+      } else if (gender === 'men' && age === '3' && wildness <= '3') {
+        $('#ian').slideDown();
+      } else if (gender === 'women' && age === '1' && wildness <= '2') {
+        $('#selena').slideDown();
+      } else if (gender === 'women' && age === '1' && wildness === '3') {
+        $('#rihanna').slideDown();
+      } else if (gender === 'women' && age === '2' && wildness <= '2') {
+        $('#gwen').slideDown();
+      } else if (gender === 'women' && age === '2' && wildness === '3') {
+        $('#sofia').slideDown();
+      } else if (gender === 'women' && age === '3' && wildness <='3') {
+        $('#heidi').slideDown();
       }
+    } else {
+      addError();
     }
-  })
-})
+  });
+});
